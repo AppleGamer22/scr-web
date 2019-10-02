ae52784638b4171534f42f2703659336431dc046
Omri Bornstein <omribor@gmail.com>
2019-10-02 22:05:10 +0930

    ionic theme

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
@@ -8,4 +7,4 @@ if (environment.production) {
 	enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(error => console.error(error));
