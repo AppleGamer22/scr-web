071e20b81a7a968290750df032e5b405925fc357
Omri Bornstein <omribor@gmail.com>
2019-10-03 20:24:30 +0930

    testing imports

--- apps/client/src/app/theme-toggle/theme-toggle.component.spec.ts
+++ apps/client/src/app/theme-toggle/theme-toggle.component.spec.ts
@@ -1,25 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ThemeToggleComponent } from "./theme-toggle.component";
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
-import { ThemeToggleComponent } from './theme-toggle.component';
+describe("ThemeToggleComponent", () => {
+	let component: ThemeToggleComponent;
+	let fixture: ComponentFixture<ThemeToggleComponent>;
 
-describe('ThemeToggleComponent', () => {
-  let component: ThemeToggleComponent;
-  let fixture: ComponentFixture<ThemeToggleComponent>;
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ThemeToggleComponent],
+			imports: [
+				FormsModule,
+				IonicModule.forRoot({scrollAssist: true})
+			]
+		}).compileComponents();
+	}));
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ThemeToggleComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ThemeToggleComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ThemeToggleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
