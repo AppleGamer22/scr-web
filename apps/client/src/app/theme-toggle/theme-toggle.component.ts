fb0dcc35c7a9139595d62328ea3b5fe32dee1e53
Omri Bornstein <omribor@gmail.com>
2019-10-04 15:48:34 +0930

    theme toggle appears when there is no preference

--- apps/client/src/app/theme-toggle/theme-toggle.component.ts
+++ apps/client/src/app/theme-toggle/theme-toggle.component.ts
@@ -7,19 +7,16 @@ import { DOCUMENT } from '@angular/common';
 	styleUrls: ["./theme-toggle.component.scss"],
 }) export class ThemeToggleComponent {
 	dark = false;
-	disabled = true;
 	constructor(@Inject(DOCUMENT) private document: Document) {
 		this.dark = JSON.parse(localStorage.getItem("dark")) || false;
-		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => this.dark = event.matches);
-		window.matchMedia("(prefers-color-scheme: no-preference)").addEventListener("change", event => this.disabled = !event.matches);
 		this.setTheme();
 	}
 	readonly setTheme = ()  => {
 		localStorage.setItem("dark", String(this.dark));
 		if (this.dark) {
-			this.document.querySelector("ion-app").classList.add("dark-theme");
+			this.document.querySelectorAll("ion-app").forEach(ionApp => ionApp.classList.add("dark-theme"));
 		} else if (!this.dark) {
-			this.document.querySelector("ion-app").classList.remove("dark-theme");
+			this.document.querySelectorAll("ion-app").forEach(ionApp => ionApp.classList.remove("dark-theme"));
 		}
 	}
 }
