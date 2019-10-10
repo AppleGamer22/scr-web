319794388616bf8c611cec675d509751a8619bea
Omri Bornstein <omribor@gmail.com>
2019-10-10 16:50:27 +1030

    npm update

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { InstagramService } from "./instagram/instagram.service";
-import { VSCOService } from "./vsco/vsco.service";
-import { InstagramController } from "./instagram/instagram.controller";
 import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
 import { ErrorFilter } from "./error.filter";
 import { LogsInterceptor } from "./logs.interceptor";
+import { AppController } from "./app.controller";
+import { InstagramService } from "./instagram/instagram.service";
+import { InstagramController } from "./instagram/instagram.controller";
+import { VSCOService } from "./vsco/vsco.service";
 import { VSCOController } from "./vsco/vsco.controller";
 
 @Module({
 	imports: [],
-	controllers: [AppController, InstagramController, VSCOController],
+	controllers: [
+		AppController,
+		InstagramController,
+		VSCOController
+	],
 	providers: [
 		{
 			provide: APP_FILTER,
--- apps/server/src/app/error.filter.ts
+++ apps/server/src/app/error.filter.ts
@@ -1,11 +1,11 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger, HttpStatus } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from "@nestjs/common";
 import { Request, Response } from "express";
 
 @Catch() export class ErrorFilter implements ExceptionFilter {
 	catch(exception: HttpException, host: ArgumentsHost) {
 		const request = host.switchToHttp().getRequest<Request>();
 		const response = host.switchToHttp().getResponse<Response>();
-		Logger.error(`${HttpStatus.INTERNAL_SERVER_ERROR} ${request.method} ${request.url}`, "", "Error", true);
-		return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(exception.message);
+		Logger.error(`${exception.getStatus()} ${request.method} ${request.url}`, "", "Error", true);
+		return response.status(exception.getStatus()).json(exception.message);
 	}
 }
