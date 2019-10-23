5f53ea44be9e5a2afc5e60d50dd407fbe65b2a0d
Omri Bornstein <omribor@gmail.com>
2019-10-23 22:31:41 +1030

    highlight service and controller

--- apps/server/src/app/highlight/highlight.controller.ts
+++ apps/server/src/app/highlight/highlight.controller.ts
@@ -1,6 +1,29 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, Req, HttpStatus, HttpException, UseGuards } from "@nestjs/common";
+import { beginScrape, ScrapeRequest } from "@scr-gui/server-interfaces";
+import { Request } from "express";
 import { HighlightService } from "./highlight.service";
+import { AuthGuard } from "../auth/auth.guard";
 
 @Controller("highlight") export class HighlightController {
 	constructor(private readonly highlightService: HighlightService) {}
+
+	@Get(":highlight/:item") @UseGuards(AuthGuard) async getHighlightFile(
+		@Param("highlight") highlight: string,
+		@Param("item") item: string,
+		@Req() request: Request
+	): Promise<string> {
+		try {
+			const { browser, page } = await beginScrape((request as ScrapeRequest).user.U_ID as string);
+			const url = await this.highlightService.getHighlightFile(highlight, Number(item), browser, page);
+			await browser.close();
+			return url;
+		} catch (error) {
+			const errorMessage = error.message as string;
+			var errorCode: HttpStatus;
+			if (errorMessage.includes("to find")) errorCode = HttpStatus.NOT_FOUND;
+			if (errorMessage.includes("to process")) errorCode = HttpStatus.INTERNAL_SERVER_ERROR;
+			throw new HttpException(error.message, errorCode);
+		}
+	}
+
 }
--- apps/server/src/app/highlight/highlight.service.ts
+++ apps/server/src/app/highlight/highlight.service.ts
@@ -1,3 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { Browser, Page } from "puppeteer";
 
-@Injectable() export class HighlightService {}
+@Injectable() export class HighlightService {
+	async getHighlightFile(highlight: string, item: number, browser: Browser, page: Page): Promise<string> {
+		try {
+			await page.goto(`https://www.instagram.com/stories/highlights/${highlight}`, {waitUntil: "domcontentloaded"});
+			const potentialErrorMessage: string = await (await (await page.$("body"))!.getProperty("textContent")).jsonValue();
+			if (potentialErrorMessage.includes("Oops, an error occurred.")) {
+				await browser.close();
+				throw new Error(`Failed to find highlight ${highlight}`);
+			}
+			for (var i = 0; i < item - 1; i += 1) {
+				await page.waitForSelector("div.coreSpriteRightChevron", {visible: true});
+				await page.click("div.coreSpriteRightChevron");
+			}
+			await page.waitForSelector("div.qbCDp", {visible: true});
+			const videoURLs = await page.$$eval("video > source", sources => sources.map(source => source.getAttribute("src")));
+			if (videoURLs) return videoURLs[0];
+			const imageURLs = await page.$$eval("div.qbCDp > img", images => images.map(image => image.getAttribute("srcset")));
+			if (imageURLs) return imageURLs[0].split(",")[0].split(" ")[0];
+		} catch (error) {
+			console.error(error.message);
+			throw new Error(error.message);//"Failed to process requested highlight file."
+		}
+	}
+}
