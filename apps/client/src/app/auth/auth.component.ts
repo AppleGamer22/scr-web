ae3f44d65ad7bd8fc78b8869b69d963d62d51f1e
Omri Bornstein <omribor@gmail.com>
2019-10-27 20:45:06 +1030

    initial instagram auth logic

--- apps/client/src/app/auth/auth.component.ts
+++ apps/client/src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { User } from "@scr-gui/server-schemas";
 
 @Component({
 	selector: "scr-gui-auth",
@@ -6,10 +8,44 @@ import { Component } from "@angular/core";
 	styleUrls: ["./auth.component.scss"],
 }) export class AuthComponent {
 	authOption = "sign-up";
-	platform: string;
-	username: string;
-	password: string
-	constructor() {}
-	signUp() {}
-	signIn() {}
+	platform = "instagram";
+	usernameField: string;
+	passwordField: string;
+	processing = false;
+	constructor(private readonly http: HttpClient) {}
+	async signUp(username: string, password: string) {
+		this.processing = true;
+		try {
+			if (username && password) {
+				const body = { username, password };
+				const user = await this.http.patch<User>("/api/auth/sign_up/instagram", body).toPromise();
+				this.processing = false;
+				if (user !== undefined) {
+					console.log("Signed-up.");
+					await this.signIn(username, password);
+					this.processing = false;
+				}
+			}
+		} catch (error) {
+			this.processing = false;
+			console.error((error as Error).message);
+		}
+	}
+	async signIn(username: string, password: string) {
+		this.processing = true;
+		try {
+			if (username && password) {
+				const body = { username, password };
+				const { token } = await this.http.patch<{token: string}>("/api/auth/sign_in/instagram", body).toPromise();
+				if (token !== undefined) {
+					localStorage.setItem("instagram", token);
+					console.log("Authenticated successfully.");
+					this.processing = false;
+				}
+			}
+		} catch (error) {
+			this.processing = false;
+			console.error((error as Error).message);
+		}
+	}
 }
