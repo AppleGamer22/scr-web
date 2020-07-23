700f49ccbc4cc7e43a59713797a7cf03e978c7a2
Omri Bornstein <omribor@gmail.com>
2020-07-23 18:35:18 +1000

    ng update @angular/cli @angular/core @nrwl/workspace rxjs

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
 	name: "server",
 	preset: "../../jest.config.js",
 	coverageDirectory: "../../coverage/apps/server",
-	testEnvironment: "node"
+	testEnvironment: "node",
+	globals: { "ts-jest": { tsConfig: "<rootDir>/tsconfig.spec.json" } },
 };
