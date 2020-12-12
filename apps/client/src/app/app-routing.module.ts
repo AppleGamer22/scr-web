70b9d31cf4938e93e21747beac8cc5a5e4140fcd
Omri Bornstein <omribor@gmail.com>
2020-12-12 12:14:41 +1100

    ng update @angular/cli @angular/core @nrwl/workspace --force

--- apps/client/src/app/app-routing.module.ts
+++ apps/client/src/app/app-routing.module.ts
@@ -4,36 +4,55 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
 	{
 		path: "",
-		loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
-	},{
+		loadChildren: () =>
+			import("./auth/auth.module").then(m => m.AuthModule),
+	},
+	{
 		path: "instagram",
-		loadChildren: () => import("./instagram/instagram.module").then(m => m.InstagramModule)
-	},{
+		loadChildren: () =>
+			import("./instagram/instagram.module").then(m => m.InstagramModule),
+	},
+	{
 		path: "highlight",
-		loadChildren: () => import("./highlight/highlight.module").then(m => m.HighlightModule)
-	},{
+		loadChildren: () =>
+			import("./highlight/highlight.module").then(m => m.HighlightModule),
+	},
+	{
 		path: "story",
-		loadChildren: () => import("./story/story.module").then(m => m.StoryModule)
-	},{
+		loadChildren: () =>
+			import("./story/story.module").then(m => m.StoryModule),
+	},
+	{
 		path: "tiktok",
-		loadChildren: () => import("./tiktok/tiktok.module").then(m => m.TikTokModule)
-	},{
+		loadChildren: () =>
+			import("./tiktok/tiktok.module").then(m => m.TikTokModule),
+	},
+	{
 		path: "vsco",
-		loadChildren: () => import("./vsco/vsco.module").then(m => m.VSCOModule)
-	},{
+		loadChildren: () =>
+			import("./vsco/vsco.module").then(m => m.VSCOModule),
+	},
+	{
 		path: "history",
-		loadChildren: () => import("./history/history.module").then(m => m.HistoryModule)
-	},{
+		loadChildren: () =>
+			import("./history/history.module").then(m => m.HistoryModule),
+	},
+	{
 		path: "license",
-		loadChildren: () => import("./license/license.module").then(m => m.LicenseModule)
-	}
+		loadChildren: () =>
+			import("./license/license.module").then(m => m.LicenseModule),
+	},
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, {
-		scrollPositionRestoration: "enabled",
-		anchorScrolling: "enabled",
-		onSameUrlNavigation: "ignore"
-	})],
-	exports: [RouterModule]
-}) export class AppRoutingModule {}
+	imports: [
+		RouterModule.forRoot(routes, {
+			scrollPositionRestoration: "enabled",
+			anchorScrolling: "enabled",
+			onSameUrlNavigation: "ignore",
+			relativeLinkResolution: "legacy",
+		}),
+	],
+	exports: [RouterModule],
+})
+export class AppRoutingModule {}
