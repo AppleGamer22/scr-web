59b748e5584775b91bf946f21d8c160a34933dac
Omri Bornstein <omribor@gmail.com>
2020-12-15 17:11:37 +1100

    initial image editor

--- apps/client/src/app/urls/urls.component.ts
+++ apps/client/src/app/urls/urls.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, DoCheck } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { History } from "@scr-web/server-schemas";
+import { ImageCroppedEvent, ImageTransform } from "ngx-image-cropper";
 
 @Component({
 	selector: "scr-web-urls",
@@ -8,7 +9,17 @@ import { History } from "@scr-web/server-schemas";
 	styleUrls: ["./urls.component.scss"],
 }) export class URLsComponent {
 	@Input() urls: string[];
+	transforms: ImageTransform[];
+	checked = false;
 	constructor(private readonly http: HttpClient) {}
+
+	ngDoCheck() {
+		if (!this.checked && this.urls.length > 0) {
+			this.transforms = new Array<ImageTransform>(this.urls.length).fill({rotate: 0, flipH: true});
+			this.checked = true;
+			console.log(this.transforms);
+		}
+	}
 
 	async deleteFile(url: string) {
 		try {
@@ -17,4 +28,13 @@ import { History } from "@scr-web/server-schemas";
 			console.error(error);
 		}
 	}
+
+	finishEdit(event: ImageCroppedEvent) {
+		console.log(event.base64);
+	}
+
+	rotateRight(i: number) {
+		this.transforms[i].flipH = !this.transforms[i].flipH;
+	}
+
 }
