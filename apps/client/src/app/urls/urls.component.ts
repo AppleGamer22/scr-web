292c62327ad5e2bead9a3085be228c5a009f8fb0
Omri Bornstein <omribor@gmail.com>
2021-01-23 13:26:17 +1100

    urls component link shorteing

--- apps/client/src/app/urls/urls.component.ts
+++ apps/client/src/app/urls/urls.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, DoCheck } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { History } from "@scr-web/server-schemas";
-import { ImageCropperSetting } from "angular-cropperjs";
 import { environment } from "../../environments/environment";
 import { ToastService } from "../toast.service";
 
@@ -11,17 +10,8 @@ import { ToastService } from "../toast.service";
 	styleUrls: ["./urls.component.scss"],
 }) export class URLsComponent {
 	@Input() urls: string[];
-	// transforms: ImageTransform[];
 	checked = false;
 	constructor(private readonly http: HttpClient, readonly toast: ToastService) {}
-
-	// ngDoCheck() {
-	// 	if (!this.checked && this.urls.length > 0) {
-	// 		this.transforms = new Array<ImageTransform>(this.urls.length).fill({rotate: 0, flipH: true});
-	// 		this.checked = true;
-	// 		console.log(this.transforms);
-	// 	}
-	// }
 
 	async deleteFile(url: string) {
 		try {
@@ -31,13 +21,4 @@ import { ToastService } from "../toast.service";
 			this.toast.showToast(error, "danger");
 		}
 	}
-
-	// finishEdit(event: ImageCroppedEvent) {
-	// 	console.log(event.base64);
-	// }
-
-	// rotateRight(i: number) {
-	// 	this.transforms[i].flipH = !this.transforms[i].flipH;
-	// }
-
 }
