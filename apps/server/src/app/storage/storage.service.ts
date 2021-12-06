c7e0b563aa9117ae44ec2366889fc12b399afdd2
Omri Bornstein <omribor@gmail.com>
2021-12-06 13:26:42 +1100

    uses buffer to save file from url, deletes empty folders

--- apps/server/src/app/storage/storage.service.ts
+++ apps/server/src/app/storage/storage.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from "@nestjs/common";
 import { HttpService } from "@nestjs/common";
 import { FileType } from "@scr-web/client-schemas";
 import { AxiosRequestConfig } from "axios";
-import { existsSync, mkdirSync, writeFileSync, rmSync } from "fs";
+import { existsSync, mkdirSync, readdirSync, rmSync, rmdirSync, createWriteStream } from "fs";
+import { join, dirname } from "path";
+import { Stream } from "stream";
 
 @Injectable() export class StorageService {
 	constructor(private readonly http: HttpService) {}
@@ -13,29 +15,21 @@ import { existsSync, mkdirSync, writeFileSync, rmSync } from "fs";
 	 * @param file file name
 	 * @param data file contents (Buffer)
 	 */
-	addFileFromBuffer(type: FileType, directory: string, file: string, data: Buffer) {
-		const directoryPath = `${process.cwd()}/storage/${type}/${directory}`;
-		const fullPath = `${directoryPath}/${file}`;
-		try {
-			if (!existsSync(directoryPath)) mkdirSync(directoryPath, {recursive: true});
-			if (!existsSync(fullPath)) writeFileSync(fullPath, data, {encoding: "binary"});
-		} catch (error) {
-			console.error(error);
-			throw new Error(`Could not save file ${type}/${directory}/${file}`);
-		}
-	}
-	/**
-	 * saves file to file system from URL
-	 * @param type file source
-	 * @param directory directory name
-	 * @param file file name
-	 * @param url file contents' URL
-	 */
-	async addFileFromURL(type: FileType, directory: string, file: string, url: string, tiktok = false, cookies?: string, userAgent?: string) {
-		const directoryPath = `${process.cwd()}/storage/${type}/${directory}`;
-		const fullPath = `${directoryPath}/${file}`;
-		try {
-			let config: AxiosRequestConfig = {responseType: "arraybuffer"}
+	// addFileFromBuffer(type: FileType, directory: string, file: string, data: Buffer) {
+	// 	const directoryPath = join(process.cwd(), "storage", type, directory);
+	// 	const fullPath = join(directoryPath, file);
+	// 	try {
+	// 		if (!existsSync(directoryPath)) mkdirSync(directoryPath, {recursive: true});
+	// 		if (!existsSync(fullPath)) writeFileSync(fullPath, data, {encoding: "binary"});
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 		throw new Error(`Could not save file ${type}/${directory}/${file}`);
+	// 	}
+	// }
+
+	private downloadFileFromURLBuffer(path: string, url: string, tiktok = false, cookies?: string, userAgent?: string): Promise<void> {
+		return new Promise(async (resolve, reject) => {
+			let config: AxiosRequestConfig = {responseType: "stream"}
 			if (tiktok && cookies !== undefined) {
 				config.headers = {
 					Cookie: cookies,
@@ -44,15 +38,39 @@ import { existsSync, mkdirSync, writeFileSync, rmSync } from "fs";
 					Referer: "https://www.tiktok.com/"
 				};
 			}
-			// if (userAgent !== undefined) {
-			// 	config.headers = {
-			// 		...config.headers,
-			// 		"User-Agent": userAgent
-			// 	}
-			// }
-			const { data } = await this.http.get(url, config).toPromise();
+			const writer = createWriteStream(path);
+			this.http.get<Stream>(url, config).toPromise().then(response => {
+				response.data.pipe(writer);
+			}).catch(err => {
+				writer.close();
+				rmSync(path);
+				reject(err);
+			});
+			writer.on("error", err => {
+				writer.close();
+				rmSync(path);
+				reject(err);
+			});
+			writer.on("finish", () => {
+				writer.close();
+				resolve();
+			});
+		});
+	}
+
+	/**
+	 * saves file to file system from URL
+	 * @param type file source
+	 * @param directory directory name
+	 * @param file file name
+	 * @param url file contents' URL
+	 */
+	async addFileFromURL(type: FileType, directory: string, file: string, url: string, tiktok = false, cookies?: string, userAgent?: string) {
+		const directoryPath = join(process.cwd(), "storage", type, directory);
+		const fullPath = join(directoryPath, file);
+		try {
 			if (!existsSync(directoryPath)) mkdirSync(directoryPath, {recursive: true});
-			if (!existsSync(fullPath)) writeFileSync(fullPath, data, {encoding: "binary"});
+			await this.downloadFileFromURLBuffer(fullPath, url, tiktok, cookies);
 		} catch (error) {
 			console.error(error);
 			throw new Error(`Could not save file ${type}/${directory}/${file}`);
@@ -65,9 +83,15 @@ import { existsSync, mkdirSync, writeFileSync, rmSync } from "fs";
 	 * @param file file name
 	 */
 	removeFile(type: FileType, directory: string, file: string) {
-		const fullPath = `${process.cwd()}/storage/${type}/${directory}/${file}`;
+		const directoryPath = join(process.cwd(), "storage", type, directory);
+		const fullPath = join(directoryPath, file);
 		try {
-			if (existsSync(fullPath)) rmSync(fullPath);
+			if (existsSync(fullPath)) {
+				rmSync(fullPath);
+				if (readdirSync(directoryPath).length === 0) {
+					rmdirSync(directoryPath);
+				}
+			}
 		} catch (error) {
 			console.error(error);
 			throw new Error(`Could not remove file ${type}/${directory}/${file}`);
