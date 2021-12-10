64b3a8c9a72b0220ef318066bd8d6fcf3f4c65dd
Omri Bornstein <omribor@gmail.com>
2021-12-10 14:23:22 +1100

    tiktok scraping (undo) fix

--- apps/server/src/app/tiktok/tiktok.service.ts
+++ apps/server/src/app/tiktok/tiktok.service.ts
@@ -2,33 +2,33 @@ import { Injectable } from "@nestjs/common";
 import { writeFileSync } from "fs";
 import { Browser, Page, Protocol } from "puppeteer-core";
 
-// interface TikTokPost {
-// 	props: {
-// 		pageProps: {
-// 			itemInfo: {
-// 				itemStruct: {
-// 					author: {
-// 						uniqueId: string
-// 					},
-// 					video: {
-// 						downloadAddr: string
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-
 interface TikTokPost {
-	ItemModule: {
-		[post: string]: {
-			author: string,
-			video: {
-				downloadAddr: string
+	props: {
+		pageProps: {
+			itemInfo: {
+				itemStruct: {
+					author: {
+						uniqueId: string
+					},
+					video: {
+						downloadAddr: string
+					}
+				}
 			}
 		}
 	}
 }
+
+// interface TikTokPost {
+// 	ItemModule: {
+// 		[post: string]: {
+// 			author: string,
+// 			video: {
+// 				downloadAddr: string
+// 			}
+// 		}
+// 	}
+// }
 
 @Injectable() export class TikTokService {
 	/**
@@ -45,16 +45,20 @@ interface TikTokPost {
 				await browser.close();
 				throw new Error(`Failed to find post ${id}`);
 			}
-			const data: TikTokPost = await page.evaluate(() => window["SIGI_STATE"])
-			if (!data) {
-				await browser.close();
-				throw new Error(`Failed to find post ${id}`);
-			}
+			// const data: TikTokPost = await page.evaluate(() => window["SIGI_STATE"]);
+			// if (!data) {
+			// 	await browser.close();
+			// 	throw new Error(`Failed to find post ${id}`);
+			// }
+			const script = await page.evaluate(() => (document.getElementById("__NEXT_DATA__") as HTMLScriptElement).text);
+			const json: TikTokPost = JSON.parse(script);
 			// @ts-ignore
 			const { cookies } = await page._client.send("Network.getAllCookies") as {cookies: Protocol.Network.Cookie[]};
 			return {
-				urls: [data.ItemModule[id.split("/")[2]].video.downloadAddr],
-				username: data.ItemModule[id.split("/")[2]].author,
+				urls: [json.props.pageProps.itemInfo.itemStruct.video.downloadAddr],
+				username: json.props.pageProps.itemInfo.itemStruct.author.uniqueId,
+				// urls: [data.ItemModule[id.split("/")[2]].video.downloadAddr],
+				// username: data.ItemModule[id.split("/")[2]].author,
 				cookies: cookies.filter(cookie => cookie.domain.includes("tiktok.com")).map(cookie => `${cookie.name}=${cookie.value}`).join("; ")
 			};
 		} catch (error) {
